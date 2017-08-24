16a02aee58432c50b17bd5aec978d9387546e42a
Wei Yuanmin <wey1sgp@bosi-eng.global>
2017-08-24 13:47:48 +0800

    Updated function pointer part and added coding styles.

--- CPlusPlus/function pointer.cpp
+++ CPlusPlus/function pointer.cpp
@@ -1,64 +1,94 @@
 #include <iostream>
 using namespace std;
 
+// https://isocpp.org/wiki/faq/pointers-to-members#memfnptr-vs-fnptr
 // define a function pointer type
-typedef int(*sum)(int, int);
+typedef int(*callbackptrtype)(int, int);
 
 // define a function pointer type
-typedef int(callback)(int, int);
+typedef int(callbacktype)(int, int);
 
 // define a function pointer variable
-int(*sum3)(int, int);
+int(*callbackptr)(int, int);
 
+void my0(callbackptrtype cb, int a, int b)
+{
+    cout << "my0 return: " << cb(a, b) << endl;
+}
+
+void my1(callbacktype cb, int a, int b)
+{
+    cout << "my2 return: " << cb(a, b) << endl;
+}
+
+void my2(callbacktype* cb, int a, int b)
+{
+    cout << "my3 return: " << cb(a, b) << endl;
+}
 
 int getSum(int a, int b)
 {
-	return a + b;
+    return a + b;
 };
 
-
-void my0(sum cb, int a, int b)
+class A
 {
-	
-	cout << "my0 return: " << cb(a, b) << endl;
-}
+public:
+    A()
+    {
+        // Simply cannnot do this: Assign C++ member function to C function pointer
+        //my1(getSum, 1, 2);
 
-//void my1(sum *cb, int a, int b)
-//{
-//	cb(a, b);
-//}
+        // OK to use static method
+        my1(getSum2, 1, 2);
+    }
 
-void my2(callback cb, int a, int b)
-{
-	cout << "my2 return: " << cb(a, b) << endl;
-}
+    void my0(callbackptrtype cb, int a, int b)
+    {
+        cout << "my0 return: " << cb(a, b) << endl;
+    }
 
-void my3(callback* cb, int a, int b)
-{
-	cout << "my3 return: " << cb(a, b) << endl;
-}
+    void my1(callbacktype cb, int a, int b)
+    {
+        cout << "my2 return: " << cb(a, b) << endl;
+    }
 
+    void my2(callbacktype* cb, int a, int b)
+    {
+        cout << "my3 return: " << cb(a, b) << endl;
+    }
+private:
+    int getSum(int a, int b)
+    {
+        return a + b;
+    };
+
+    static int getSum2(int a, int b)
+    {
+        return a + b;
+    };
+};
 int main()
 {
-	sum a = getSum; // correct, sum is a type
-	sum a = &getSum; // correct, sum is a type
-	//sum *b = getSum; // wrong
+    callbackptrtype a0 = getSum; // correct, callbackptrtype is a type
+    callbackptrtype a1 = &getSum; // correct, callbackptrtype is a type
+    //callbackptrtype *b = getSum; // wrong
 
-	//callback b = getSum; //wrong, sum2 is not a type
-	callback *b = getSum; // correct
-	callback *b = &getSum; // correct
+    //callbackptrtype b = getSum; //wrong
+    callbacktype *b = getSum; // correct
+    callbacktype *b = &getSum; // correct
 
-	//sum3 a = getSum; // wrong, sum3 is not a type
-	sum3 = getSum; // correct, sum3 is a variable
-	sum3 = &getSum; // correct, sum3 is a variable
+    //callbackptr a = getSum; // wrong, callbackptr is not a type
+    callbackptr = getSum; // correct, callbackptr is a variable
+    callbackptr = &getSum; // correct, callbackptr is a variable
 
-	my0(getSum, 1, 2);
-	my0(&getSum, 1, 2);
+    my0(getSum, 1, 2);
+    my0(&getSum, 1, 2);
 
-	my2(getSum, 1, 2);
-	my2(&getSum, 1, 2);
+    my1(getSum, 1, 2);
+    my1(&getSum, 1, 2);
 
-	my3(getSum, 1, 2);
-	my3(&getSum, 1, 2);
-	return 0;
+    my2(getSum, 1, 2);
+    my2(&getSum, 1, 2);
+    return 0;
 }
